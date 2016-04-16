82adc3c1f79ccaf4533c328515c4e649af5c7985
ygbekou <ericgbekou@hotmail.com>
2016-04-15 23:19:01 -0500

    DelegatingFilter

---
+++ src/com/ukad/listener/DelegationFilter.java
@@ -0,0 +1,34 @@
+package com.ukad.listener;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Enumeration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import com.ukad.security.model.SessionHistory;
+
+public class DelegationFilter implements Filter {
+
+	FilterConfig filterConfig = null;
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+	}
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+		filterChain.doFilter(request, response);
+
+	}
+
+}
