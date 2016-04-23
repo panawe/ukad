d5fa68c9571d78b1e425f675931802acc186e18c
ygbekou <ericgbekou@hotmail.com>
2016-04-22 22:35:35 -0500

    Commiting

---
+++ src/com/ukad/model/Project.java
@@ -0,0 +1,164 @@
+package com.ukad.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "PROJECT")
+public class Project extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "PROJECT_ID")
+	private Long id;
+
+	@Column(name = "TITLE")
+	private String title;
+
+	@Column(name = "DESCRIPTION")
+	private String description;
+
+	@Column(name = "SPONSORS")
+	private String sponsors;
+
+	@Column(name = "BUDGET")
+	private Double budget;
+
+	@JsonFormat(pattern = "MM/dd/yyyy")
+	@Column(name = "PROJECTED_START_DATE")
+	private Date projectedStartDate;
+
+	@JsonFormat(pattern = "MM/dd/yyyy")
+	@Column(name = "PROJECTED_END_DATE")
+	private Date projectedEndDate;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+	@Column(name = "START_DATE")
+	private Date startDate;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+	@Column(name = "END_DATE")
+	private Date endDate;
+
+	@Column(name = "STATUS")
+	private Short status;
+
+	@Transient
+	private boolean hasPhoto;
+
+	@Transient
+	public String getStatusDescription() {
+		String desc = "";
+		if (status == 0)
+			desc = "Pas Commencee";
+		else if (status == 1)
+			desc = "En cours";
+		else if (status == 2)
+			desc = "Annulee";
+		else if (status == 3)
+			desc = "Terminee";
+		else if (status == 4)
+			desc = "Reportee";
+
+		return desc;
+	}
+
+	public boolean isHasPhoto() {
+		return hasPhoto;
+	}
+
+	public void setHasPhoto(boolean hasPhoto) {
+		this.hasPhoto = hasPhoto;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSponsors() {
+		return sponsors;
+	}
+
+	public void setSponsors(String sponsors) {
+		this.sponsors = sponsors;
+	}
+
+	public Double getBudget() {
+		return budget;
+	}
+
+	public void setBudget(Double budget) {
+		this.budget = budget;
+	}
+
+	public Date getProjectedStartDate() {
+		return projectedStartDate;
+	}
+
+	public void setProjectedStartDate(Date projectedStartDate) {
+		this.projectedStartDate = projectedStartDate;
+	}
+
+	public Date getProjectedEndDate() {
+		return projectedEndDate;
+	}
+
+	public void setProjectedEndDate(Date projectedEndDate) {
+		this.projectedEndDate = projectedEndDate;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Short getStatus() {
+		return status;
+	}
+
+	public void setStatus(Short status) {
+		this.status = status;
+	}
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
